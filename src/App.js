5be3051633aa84ba6f5ea980304dbc1852b31541
kasheesh <kashishbhurani@gmail.com>
2023-07-24 20:31:19 +0530

    Done with the login and sign up page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Start from "./components/Start";
 import HomePage from "./components/HomePage";
 import LoginForm from "./components/Login";
+import LoginFor from "./components/Signup";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./styles/Home.css";
 
@@ -14,6 +15,9 @@ function App() {
         <Route path="/" element={<Start />} />
         <Route path="/other" element={<HomePage />} />
         <Route path="/login" element={<LoginForm />} />
+        <Route path="/signup" element={<LoginFor />} />
+        <Route path="/backtologin" element={<LoginForm />} />
+       
 
         {/* Add more routes as needed */}
       </Routes>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import login from '../styles/login.module.css'; // Make sure to have the CSS file in the same directory
-import petlove from "../assets/pet-love.png"
+import feedinganimal from "../assets/feeding.png";
 import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
@@ -9,17 +9,21 @@ const LoginForm = () => {
       <div className={login.formscontainer}>
         <div className={login.signinsignup}>
           <form action="#" className={login.signinform}>
-            <h2 className={login.title}>Log in</h2>
+            <h2 className={login.title}>Sign up</h2>
             <div className={login.inputfield}>
               <i className="fas fa-user"></i>
               <input type="text" placeholder="Username" />
             </div>
             <div className={login.inputfield}>
+              <i className="fas fa-envelope"></i>
+              <input type="email" placeholder="Email" />
+            </div>
+            <div className={login.inputfield}>
               <i className="fas fa-lock"></i>
               <input type="password" placeholder="Password" />
             </div>
-            <input type="submit" value="Login" className={`${login.btn1} ${login.solid}`} />
-            <p className={login.socialtext}>Or Sign in with social platforms</p>
+            <input type="submit" className={login.btn1} value="Sign up" />
+            <p className={login.socialtext}>Or Sign up with social platforms</p>
             <div className={login.socialmedia}>
               <a href="#" className={login.socialicon}>
                 <i className="fab fa-facebook-f"></i>
@@ -37,22 +41,23 @@ const LoginForm = () => {
           </form>
         </div>
       </div>
+
       <div className={login.panelscontainer}>
-      <div className={`${login.panel} ${login.leftpanel}`}>
-        <div className={login.content}>
-          <h3>New here ?</h3>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis, ex
-            ratione. Aliquid!
-          </p>
-          <Link to = "/signup"><button className={`${login.btn1} ${login.transparent}`} id="sign-up-btn">
-            Sign up
-          </button>
-          </Link>
+        <div className={`${login.panel} ${login.leftpanel}`}>
+          <div className={login.content}>
+            <h3>One of us ?</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum laboriosam
+              ad deleniti.
+            </p>
+            <Link to= "/backtologin"><button className={`${login.btn1} ${login.transparent}`} id="sign-in-btn">
+              Log in
+            </button>
+            </Link>
+          </div>
+          <img src={feedinganimal} className={login.image} alt="" />
         </div>
-        <img src={petlove} className={login.image} alt="" />
       </div>
-    </div>
     </div>
   );
 };
