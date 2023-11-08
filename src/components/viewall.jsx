ef7125d7d85c02fe04a17eadf2f3146c787b2013
kasheesh <kashishbhurani@gmail.com>
2023-11-08 13:35:39 +0530

    Made changes to the css for view all page

--- src/components/viewall.jsx
+++ src/components/viewall.jsx
@@ -23,7 +23,112 @@ function ViewAll() {
               <h3>Beagle</h3>
             </div>
           </div>
-          {/* Repeat the above structure for other items */}
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="https://cdn.shopify.com/s/files/1/0424/4606/1723/files/Syrian-Hamster_grande.jpg?v=1594383125" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Stuart, Syrian Hamster</h3>
+              <h5>Location: New Delhi</h5>
+              <h5>contact: adoptionJ@gmail.com</h5>
+            </div>
+          </div>
+          <div className="box">
+            <div className="imgbx">
+              <img src="#" alt="Beagle Image" />
+            </div>
+            <div className="text">
+              <h3>Beagle</h3>
+            </div>
+          </div>
         </div>
       </section>
     </div>
