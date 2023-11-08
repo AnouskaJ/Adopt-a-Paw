e9d730409f910711d7aae30d32547223a8e52e76
kasheesh <kashishbhurani@gmail.com>
2023-11-08 14:43:27 +0530

    Added a button and completer the page

--- src/components/viewall.jsx
+++ src/components/viewall.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 
@@ -13,6 +14,8 @@ function ViewAll() {
             </div>
             <div className="text">
               <h3>British Shorthair</h3>
+              <h5>Location:Bangalore</h5>
+              <h5>Contact: HappyHome@gmail.com</h5>
             </div>
           </div>
           <div className="box">
@@ -156,6 +159,7 @@ function ViewAll() {
             </div>
           </div>
         </div>
+        <Link to="/other"><a href="#" className="btn">Go back</a></Link>
       </section>
     </div>
   );
