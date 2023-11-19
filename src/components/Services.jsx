2f06277e8874b608af195a83a52849dcaf78cebe
kasheesh <kashishbhurani@gmail.com>
2023-11-19 23:21:45 +0530

    project extension styling added

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -25,14 +25,16 @@ const CardComponent = () => {
         <a href="#" className="logo">Adopt-a-<span>Paw</span></a>
         <div className={`menutoggle ${isMenuActive ? 'active' : ''}`} onClick={toggleMenu}></div>
         <ul className={`navigation ${isMenuActive ? 'active' : ''}`}>
-          <li><a href="#banner">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#menu">Adopt</a></li>
+          <Link to="/other"><li><a>Home</a></li></Link>
+          <Link to="/other"><li><a>About Us</a></li></Link>
+          <Link to="/other"><li><a>Adopt</a></li></Link>
+         
           <Link to="/login"><li><a>Login</a></li></Link>
           <Link to="/services"><li><a>Services</a></li></Link>
           <li><a href="#contactus">Contact</a></li>
         </ul>
       </header>
+      
       <div className='backgroundc'>
         <h1 className='headerc'>Got some work? We will take care of your pets for you!</h1>
       <div className="cards">
@@ -50,10 +52,19 @@ const CardComponent = () => {
         <div className="img"></div>
         <div className="textBox">
           <div className="textContent">
-            <p className="h1">Leave your pets with us!</p>
-            <span className="h1 span">Available on all days </span>
+            <p className="h1">Your pet needs grooming? We are here!</p>
+            <span className="h1 span">Available on all weekdays! </span>
           </div>
-          <p className="p">Enjoy a tension-free vacation while we take care of your pets</p>
+          <p className="p">We provide all kinds of pet grooming services right at your home!</p>
+        </div>
+      </div>
+      <div className="cards">
+        <div className="img"></div>
+        <div className="textBox">
+          <div className="textContent">
+            <p className="h1">Want to become a caretaker?</p>
+          </div>
+          <p className="p">Want to become a part of the adopt-a-paw family? Register with us now!</p>
         </div>
       </div>
     </div>
